31504a294808f895439c62ed3974e020590bdd37
Latricia <tricianickelberry@gmail.com>
2020-06-03 11:38:30 -0700

    added resources for each model

---
+++ app/models/shoe.rb
@@ -0,0 +1,4 @@
+class Shoe < ApplicationRecord
+  belongs_to :brand
+  belongs_to :user
+end
---
+++ db/migrate/20200603182241_create_shoes.rb
@@ -0,0 +1,12 @@
+class CreateShoes < ActiveRecord::Migration[6.0]
+  def change
+    create_table :shoes do |t|
+      t.string :type
+      t.string :description
+      t.belongs_to :brand, null: false, foreign_key: true
+      t.belongs_to :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
