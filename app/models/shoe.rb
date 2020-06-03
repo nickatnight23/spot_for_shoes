33dd15027cc916425e2cda664f0d1859f84ab49e
Latricia <tricianickelberry@gmail.com>
2020-06-03 12:22:33 -0700

    added associations

--- app/models/shoe.rb
+++ app/models/shoe.rb
@@ -1,4 +1,6 @@
 class Shoe < ApplicationRecord
   belongs_to :brand
-  belongs_to :user
+  belongs_to :user #created it
+  has_many :reviews
+  has_many :users, through: :reviews #people who have reviewed
 end
