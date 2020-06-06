62e0d61f9c56d256dea0e629e65830cba5ecaf80
Latricia <tricianickelberry@gmail.com>
2020-06-06 13:47:18 -0700

    updated controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+
     def new
         @user = User.new
     end
@@ -6,13 +7,17 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
-            session[user_id] = @user.id
+            session[:user_id] = @user.id
             redirect_to user_path(@user)
         else
          render :new
     end
+  end
 
     def show
+        @user = User.find(params[:id])
+        redirect_to '/' if !@user
+
     end
 
     private
