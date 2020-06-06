49a2a084b9eb86cea795cb416f35ad13fcf3726b
Latricia <tricianickelberry@gmail.com>
2020-06-06 13:50:50 -0700

    updated users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
     has_many :reviews
-    has_many :reviewed_shoes, through: :reviews, source: shoe
+    has_many :reviewed_shoes, through: :reviews, source: :shoe
 
     has_many :shoes
+
+    validates :username, uniqueness: true, presence: true
+
+    has_secure_password
 end
