c17947bab558cfb003a72dc1c997ea8be21cc711
Latricia <tricianickelberry@gmail.com>
2020-06-07 16:55:13 -0700

    added delete route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   post 'signup' => 'users#create'
+  delete '/logout' => 'sessions#destroy'
   resources :reviews
   resources :shoes
   resources :brands
