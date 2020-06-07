3e8f09193de67223c235dd1196d6ec5634d47ea7
Latricia <tricianickelberry@gmail.com>
2020-06-07 16:46:52 -0700

    added create login and redirect

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,26 @@
 class SessionsController < ApplicationController
     def welcome
     end
+
+    def destroy
+        session.delete(:user_id)
+        redirect_to '/'
+    end
+
+    def new
+
+    end
+
+    def create
+      @user =  User.find_by(username: params[:user][:username])
+    #   if @user && @user.authenticate(params[:user][:password])
+
+        if @user.try(:authenticate, params[:user][:password])
+            session[:user_id] =@user.id
+            redirect_to user_path(@user)
+        else
+            flash[:error] = "login info incorrect. Please try again."
+            redirect_to login_path
+    end
+end
 end
