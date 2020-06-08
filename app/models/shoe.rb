0a17ab1f4e7c58380532481caf8484699d2f0339
Latricia <tricianickelberry@gmail.com>
2020-06-08 07:25:36 -0700

    added attributes for brand

--- app/models/shoe.rb
+++ app/models/shoe.rb
@@ -3,4 +3,6 @@ class Shoe < ApplicationRecord
   belongs_to :user #created it
   has_many :reviews
   has_many :users, through: :reviews #people who have reviewed
+
+  accepts_nested_attributes_for :brand
 end
