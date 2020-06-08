277dd2520fff19b05196b7e4f2b4efcbcecdb315
Latricia <tricianickelberry@gmail.com>
2020-06-08 07:24:46 -0700

    added private method

--- app/controllers/shoes_controller.rb
+++ app/controllers/shoes_controller.rb
@@ -1,5 +1,17 @@
 class ShoesController < ApplicationController
 
     def new
+        @shoe= Shoe.new
+        @shoe.build_brand
+    end
+
+    def create
+    end
+        
+    private
+
+    def shoe_params
+        params.require(:shoe).permit(:type, :description, :brand_id, 
+        brand_attributes:[:name])
     end
 end
