57be93e269abd311f484f2d4d95e3eee0b9cd67b
Latricia <tricianickelberry@gmail.com>
2020-06-09 07:16:42 -0700

    added index and show method

--- app/models/shoe.rb
+++ app/models/shoe.rb
@@ -4,5 +4,10 @@ class Shoe < ApplicationRecord
   has_many :reviews
   has_many :users, through: :reviews #people who have reviewed
 
-  accepts_nested_attributes_for :brand
+  # accepts_nested_attributes_for :brand
+
+  def brand_attributes=(attributes)
+    brand = Brand.find_or_create_by(
+      attributes) if !name.empty?
+  end
 end
