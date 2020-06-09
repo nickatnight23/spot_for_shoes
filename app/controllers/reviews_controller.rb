c66dfd45463671b0be9f848a60b9dd137baea940
Latricia <tricianickelberry@gmail.com>
2020-06-09 07:18:30 -0700

    added new method and index

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,12 @@
 class ReviewsController < ApplicationController
+
+    def new
+        @shoe = Shoe.find_by_id(params[:shoe_id])
+        @review = @shoe.build_review
+
+        # A review belongs_to a shoe
+    end
+
+    def index
+    end
 end
