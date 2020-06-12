30e80ecef4e6cf0c89b2ecd1e3f3e0241faa0b74
Latricia <tricianickelberry@gmail.com>
2020-06-12 07:46:00 -0700

    updated with scope

--- app/models/shoe.rb
+++ app/models/shoe.rb
@@ -5,11 +5,33 @@ class Shoe < ApplicationRecord
   has_many :users, through: :reviews #people who have reviewed
 
   validates :shoe_type, presence: true
+  validate :not_a_duplicate
   # accepts_nested_attributes_for :brand
+
+  scope :order_by_rating, ->{left_joins(:reviews).group(:id).
+  order('avg(stars)desc')}
+
+  def self.alpha #scope method
+    order(:shoe_type)
+  end
 
  def brand_attributes=(attributes)
     brand = Brand.find_or_create_by(
       attributes) if !attributes['name'].empty?
   end
+
+  def not_a_duplicate
+    # if there is already a shoe with that type and brand, 
+    #throw and error
+
+    if Shoe.find_by(shoe_type: shoe_type, brand_id: brand_id)
+      errors.add(:shoe_type, "has already been added for that brand")
+  end
 end
+
+  def shoe_type_and_brand
+    #"{shoe_type - #{brand.name}"
+  end
+end
+
 
