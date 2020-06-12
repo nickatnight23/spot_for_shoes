503f1a6953d07e92def0997076db56c506930884
Latricia <tricianickelberry@gmail.com>
2020-06-12 07:48:25 -0700

    updated model

--- app/models/review.rb
+++ app/models/review.rb
@@ -5,4 +5,6 @@ class Review < ApplicationRecord
   validates :title, presence: true
   validates :stars, numericality: {only_integer: true,
    greater_than_or_equal_to: 0, less_than: 6}
+
+   validates :shoe, uniqueness: {scope: :user, message:"has already been reviewed by you"}
 end
