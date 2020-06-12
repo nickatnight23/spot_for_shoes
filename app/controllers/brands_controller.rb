13877a2a7c3575907e890114c4f528c808300f55
Latricia <tricianickelberry@gmail.com>
2020-06-12 08:22:26 -0700

    added helper method

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -1,6 +1,7 @@
 class BrandsController < ApplicationController
+    before_action :redirect_if_not_logged_in
     
     def index
-        @brands = Brand.all
+        @brands = Brand.alpha
     end
 end
