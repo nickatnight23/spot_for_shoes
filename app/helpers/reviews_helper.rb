d0ec1c0e0fedaf90c8f9fee3b72e953fe9ae80c7
Latricia <tricianickelberry@gmail.com>
2020-06-12 14:50:09 -0700

    added helper method

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,2 +1,10 @@
 module ReviewsHelper
-end
+    def display_header(review)
+        if params[:ice_cream_id]
+            content_tag(:h1, "Add a Review for #{review.shoe.shoe_type} -  #{review.shoe.brand.name}")
+        else
+          content_tag(:h1, "Create a review")
+        end
+      end
+    end
+
