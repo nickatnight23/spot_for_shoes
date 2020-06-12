9492cf54aa949072db5085668a5120ad7e270478
Latricia <tricianickelberry@gmail.com>
2020-06-12 14:50:46 -0700

    added helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
-end
+    def render_nav_bar
+        if logged_in?
+          render partial: 'layouts/loggedin_links'
+        else
+          render partial: 'layouts/loggedout_links'
+        end
+      end
+    end
