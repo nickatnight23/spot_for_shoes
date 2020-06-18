09d6e60f82e5b9354d968602150eb19601b040a9
Latricia <tricianickelberry@gmail.com>
2020-06-18 06:53:14 -0700

    updated model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
     has_many :reviews
     has_many :reviewed_shoes, through: :reviews, source: :shoe
 
-    has_many :shoes
+    has_many :shoes, dependent: :destroy
 
     validates :username, uniqueness: true, presence: true
 
