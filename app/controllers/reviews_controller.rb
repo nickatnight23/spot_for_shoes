9cc37ed1c9a95d845baf1426c73e901346c685ed
Latricia <tricianickelberry@gmail.com>
2020-07-13 17:59:23 -0700

    updated controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,17 +2,22 @@ class ReviewsController < ApplicationController
     before_action :redirect_if_not_logged_in
 
     def new
-        if @shoe = Shoe.find_by_id(params[:shoe_id])
-            @review = @shoe.reviews.build
-          else
-            @review = Review.new
-          end
+        @review= Review.new
+        # if @shoe = Shoe.find_by_id(params[:shoe_id])
+        #     @review.build_shoe
+        #   else
+        #     @review = Review.new
+        #   end
         end
 
     def create
-       @review = current_user.reviews.build(review_params)
+    #    @review = current_user.reviews.build(review_params)
+       @review = Review.new(review_params)
+       @review.user_id = current_user.id
+       @review.shoe_id = params[:shoe_id].to_i
+       @shoe = Shoe.find_by_id(params[:shoe_id])
        if @review.save #checks for validation
-        redirect_to review_path(@review)
+        redirect_to shoe_path(@shoe)
        else
         render :new
     end
