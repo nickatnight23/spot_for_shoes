236a58f1b625690949a256c0ce0058903eeb37e1
Latricia <tricianickelberry@gmail.com>
2020-08-07 10:22:02 -0700

    updated routes non nested review

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
   # resources :brands
   resources :users
   resources :brands
+  resources :reviews, only: [:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
 
