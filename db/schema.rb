d13f5dc24d4efc788dd98c9308f36f9bdffe305e
Latricia <tricianickelberry@gmail.com>
2020-09-02 10:48:07 -0700

    updated migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_08_161238) do
+ActiveRecord::Schema.define(version: 2020_09_02_155650) do
 
   create_table "brands", force: :cascade do |t|
     t.string "name"
@@ -46,6 +46,8 @@ ActiveRecord::Schema.define(version: 2020_06_08_161238) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "provider"
+    t.string "uid"
   end
 
   add_foreign_key "reviews", "shoes"
